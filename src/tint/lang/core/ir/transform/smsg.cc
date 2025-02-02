c96a4234add68defc80213078c1cb32f7fe11d04
Reese Levine <reeselevine1@gmail.com>
2025-02-01 18:29:09 -0800

    Work on handling more complex typing and short-circuit atomic types

--- src/tint/lang/core/ir/transform/smsg.cc
+++ src/tint/lang/core/ir/transform/smsg.cc
@@ -23,16 +23,73 @@ struct State {
     /// The type manager.
     core::type::Manager& ty{ir.Types()};
 
-    const type::Type* RewriteBindingType(const type::Type* type) {
+    // Rewrite type to atomic
+    // TODO: handle structs
+    const type::Type* RewriteType(const type::Type* type) {
       return tint::Switch(type,
+        [&](const type::Pointer* ptr) {
+          return ty.ptr(ptr->AddressSpace(), RewriteType(ptr->UnwrapPtr()), ptr->Access());
+        },
+        [&](const type::Array* ar) {
+          auto newElemType = RewriteType(ar->ElemType());
+          auto typeAndCount = ar->Count();
+          return tint::Switch(typeAndCount,
+            [&](const type::ConstantArrayCount* count) {
+              return ty.array(newElemType, count->value, ar->Stride());
+            },
+            [&](const type::RuntimeArrayCount*) {
+              return ty.runtime_array(newElemType, ar->Stride());
+            },
+            TINT_ICE_ON_NO_MATCH); 
+        },
         [&](const type::I32* i32) {
+          // wrap the i32 in an atomic type
           return ty.Get<type::Atomic>(i32);
         },
         [&](const type::U32* u32) {
+          // wrap the u32 in an atomic type
+          return ty.Get<type::Atomic>(u32);
+        },
+        TINT_ICE_ON_NO_MATCH
+      );
+    }
+
+    void ForwardNewTypes(InstructionResult* res) {
+      res->ForEachUseUnsorted([&](Usage use) {
+        auto* inst = use.instruction;
+        tint::Switch(inst,
+          [&](Access* access) {
+            auto* newType = RewriteType(access->Result(0)->Type());
+            auto *innerRes = access->Result(0);
+            innerRes->SetType(newType);
+            ForwardNewTypes(innerRes);
+          },
+          [&](Load* load) {
+            auto* replacement = b.CallWithResult(load->DetachResult(), BuiltinFn::kAtomicLoad, load->From());
+            load->ReplaceWith(replacement);
+            load->Destroy();
+          },
+          TINT_ICE_ON_NO_MATCH
+        );
+      });
+    }
+
+    const type::Type* TranslateBindingType(const type::Type* type) {
+      return tint::Switch(type,
+        [&](const type::Atomic*) {
+          // atomic types are already in the correct format
+          return type;
+        },
+        [&](const type::I32* i32) {
+          // wrap the i32 in an atomic type
+          return ty.Get<type::Atomic>(i32);
+        },
+        [&](const type::U32* u32) {
+          // wrap the u32 in an atomic type
           return ty.Get<type::Atomic>(u32);
         },
         [&](const type::Array* ar) {
-          auto newElemType = RewriteBindingType(ar->ElemType());
+          auto newElemType = TranslateBindingType(ar->ElemType());
           auto typeAndCount = ar->Count();
           return tint::Switch(typeAndCount,
             [&](const type::ConstantArrayCount* count) {
@@ -47,66 +104,51 @@ struct State {
       );
     }
 
-    void HandleBindingPoint(Var* bp) {
+    void TranslateBindingPoint(Var* bp) {
       auto* oldPtr = bp->Result(0)->Type()->As<type::Pointer>();
       TINT_ASSERT(oldPtr);
 
-      auto* newPtr = ty.ptr(oldPtr->AddressSpace(), RewriteBindingType(oldPtr->UnwrapPtr()), oldPtr->Access());
+      auto* newPtr = ty.ptr(oldPtr->AddressSpace(), TranslateBindingType(oldPtr->UnwrapPtr()), oldPtr->Access());
       bp->Result(0)->SetType(newPtr);
-      bp->Result(0)->ForEachUseUnsorted([&](Usage use) {
-        auto* inst = use.instruction;
-        auto* access = inst->As<Access>();
-        if (!access) {
-          TINT_ICE() << "binding point usage is not an access\n";
-        }
-        auto* result = access->Result(0);
-        tint::Switch(newPtr->UnwrapPtr(),
-          [&](const type::Array* ar) {
-            auto* newAccessType = ty.ptr(newPtr->AddressSpace(), ar->ElemType(), newPtr->Access());
-            result->SetType(newAccessType);
-            result->ForEachUseUnsorted([&](Usage innerUse) {
-              auto* innerInst = innerUse.instruction;
-              auto* innerAccess = innerInst->As<Load>();
-              if (!innerAccess) {
-                TINT_ICE() << "access usage is not a load\n";
-              }
-              auto* replacement = b.CallWithResult(innerAccess->DetachResult(), BuiltinFn::kAtomicLoad, innerAccess->From());
-              innerInst->ReplaceWith(replacement);
-              innerInst->Destroy();
-            });
-          }
-        );
-      });
+      ForwardNewTypes(bp->Result(0));
     }
 
-    void VisitInstruction(Instruction* inst) {
+    // Visit an instruction which is an index/data dependency of some access
+    // TODO: Control dependencies, maybe don't fit in here?
+    void VisitIDDInst(Instruction* inst) {
       tint::Switch(inst,
         // TODO
         // we can ignore the indices of the access, because they will be handled by a separate visitor
         [&](Access *a) {
-          VisitValue(a->Object());
+          VisitIDDValue(a->Object());
         },
 	      [&](Binary *i) {
-          VisitValue(i->LHS());
-          VisitValue(i->RHS());
+          VisitIDDValue(i->LHS());
+          VisitIDDValue(i->RHS());
         },
         // TODO
         [&](Bitcast *bt) {
         },
-        // TODO think about recursion into call
+        [&](CoreBuiltinCall *cbc) {
+          // we can short circuit if the value is already loaded atomically
+          if (cbc->Func() == BuiltinFn::kAtomicLoad) {
+            return;
+          }
+        },
+        // TODO think about jumping into call
         [&](Call *c) {
           for(auto* a: c->Args()) {
-            VisitValue(a);
+            VisitIDDValue(a);
           }
         },
 	      [&](Let *i) {
-          VisitValue(i->Value());
+          VisitIDDValue(i->Value());
 	      },
         // TODO
         [&](LoadVectorElement *lve) {
         },
 	      [&](Load *l) { 
-          VisitValue(l->From());
+          VisitIDDValue(l->From());
         },
         // TODO
         [&](Unary *u) {
@@ -114,39 +156,40 @@ struct State {
         // TODO
         [&](Var *v) {
           if (v->BindingPoint()) {
-            std::cout << "root var\n";
-            HandleBindingPoint(v);
+            // Binding points are where atomic translation might need to take place
+            TranslateBindingPoint(v);
           } else {
-            VisitValue(v->Initializer());
+            VisitIDDValue(v->Initializer());
           }
         },
         TINT_ICE_ON_NO_MATCH
 	    );
     }
 
-    void VisitValue(Value* i) {
-      if (auto* c = i->As<Constant>()) {
-        std::cout << c->Value()->ValueAs<uint32_t>() << "\n";
-      } else if (auto* r = i->As<InstructionResult>()) {
-        VisitInstruction(r->Instruction());
+    // Visit index or data dependent values
+    void VisitIDDValue(Value* i) {
+      if (auto* r = i->As<InstructionResult>()) {
+        // we need to follow the chain of this index dependency
+        VisitIDDInst(r->Instruction());
+      } else if (auto* c = i->As<Constant>()) {
+        // we can safely ignore constants
+        return;
       } else {
         TINT_ICE() << "unhandled value\n";
       }
     }
 
+    // Visiting an access simply means visiting its index dependencies
     void VisitAccess(Access *a) {
-      std::cout << ir.NameOf(a->Object()).Name() << "\n"; 
       for (auto i : a->Indices()) {
-        VisitValue(i);
+        VisitIDDValue(i);
       }
     }
 
+    // Traverse the compute entry point looking for accesses.
+    // TODO: Interprocedural analysis
     void VisitComputeEntryPoint(Function* f) {
-      std::cout << ir.NameOf(f).Name() << std::endl;
-      if(f->Stage() != Function::PipelineStage::kCompute)
-        return;
       Traverse(f->Block(), [&](Access* a) {
-        std::cout << "access: \n";
         VisitAccess(a);
       });
     }
@@ -158,9 +201,17 @@ struct State {
       std::cout << before.Plain();
       std::cout << "\n\n";
 
+      bool entryPointFound = false;
       for(auto *f: ir.DependencyOrderedFunctions()) {
-        VisitComputeEntryPoint(f);
+        if (f->Stage() == Function::PipelineStage::kCompute) {
+          if (entryPointFound) {
+            TINT_ICE() << "multiple compute entry points found\n";
+          }
+          entryPointFound = true;
+          VisitComputeEntryPoint(f);
+        }
       }
+
       auto after = Disassembler(ir);
       std::cout << "// Shader After:\n";
       std::cout << after.Plain();
